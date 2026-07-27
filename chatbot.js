fffa1030b8f9f984384ad6063476d9d71d92cc46
Darshan N <darshannahata5555@gmail.com>
2026-07-27 20:16:07 +0530

    Replace AMC names with logos

--- chatbot.js
+++ chatbot.js
@@ -1,6 +1,6 @@
 (() => {
   if (!document.querySelector('link[href*="enhancements.css"]')) {
-    const stylesheet=document.createElement("link");stylesheet.rel="stylesheet";stylesheet.href="./enhancements.css?v=18";document.head.appendChild(stylesheet);
+    const stylesheet=document.createElement("link");stylesheet.rel="stylesheet";stylesheet.href="./enhancements.css?v=19";document.head.appendChild(stylesheet);
   }
   if (!document.querySelector('script[src*="enhancements.js"]')) {
     const enhancementScript=document.createElement("script");enhancementScript.src="./enhancements.js?v=6";enhancementScript.async=false;document.head.appendChild(enhancementScript);
