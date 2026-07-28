d8b6432b3851e378f04754827253ce6f74c75a52
Darshan N <darshannahata5555@gmail.com>
2026-07-28 11:26:24 +0530

    Improve mobile why choose cards

--- chatbot.js
+++ chatbot.js
@@ -1,6 +1,6 @@
 (() => {
   if (!document.querySelector('link[href*="enhancements.css"]')) {
-    const stylesheet=document.createElement("link");stylesheet.rel="stylesheet";stylesheet.href="./enhancements.css?v=21";document.head.appendChild(stylesheet);
+    const stylesheet=document.createElement("link");stylesheet.rel="stylesheet";stylesheet.href="./enhancements.css?v=22";document.head.appendChild(stylesheet);
   }
   if (!document.querySelector('script[src*="enhancements.js"]')) {
     const enhancementScript=document.createElement("script");enhancementScript.src="./enhancements.js?v=10";enhancementScript.async=false;document.head.appendChild(enhancementScript);
